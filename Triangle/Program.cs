995c7dacdda4ca4db027f0288d800ff566c063ca
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-13 11:30:30 +0200

    Add project files.

---
+++ Triangle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of rows you wish to see in pascal triangle 5");
+            Console.WriteLine();
+            Console.WriteLine("        1         ");
+            Console.WriteLine("      1    1       ");
+            Console.WriteLine("    1   2   1      ");
+            Console.WriteLine("  1   3   3   1    ");
+            Console.WriteLine("1  4    6   4   1  ");
+            Console.ReadLine();
+        }
+    }
+}
